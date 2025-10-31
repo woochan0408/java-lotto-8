69d816aecf3be83e7755c73886ce15e4712393a3
woochan0408 <tesabel48@naver.com>
2025-10-31 13:01:04 +0900

    feat(input): implement user amount input and validation logic

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,30 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        while(true){
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                String input = Console.readLine();
+                int amount = Integer.parseInt(input);
+                validationAmount(amount);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
+    public static void validationAmount(int amount) {
+        if(amount < 0) {
+            throw new IllegalArgumentException("[ERROR] 음수는 입력할 수 없습니다.");
+        }
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000으로 나누어떨어지는 금액을 입력해주세요.");
+        }
+    }
+
 }
